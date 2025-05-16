80acd6d6cdd6ae317f19767bf9bfe7799aa3648f
TepNik <tepelin.nikita@gmail.com>
2025-05-16 15:13:25 +0400

    Fixed typos and bad english

--- src/PoolConfiguratorWithCustomInitialize.sol
+++ src/PoolConfiguratorWithCustomInitialize.sol
@@ -9,9 +9,12 @@ contract PoolConfiguratorWithCustomInitialize is PoolConfiguratorInstance {
   function initialize(IPoolAddressesProvider provider) public virtual override initializer {
     super.initialize(provider);
 
-    // @note should be called before the v3.4 upgrade of the Pool contract in order to
-    //       fetch the value from the storage. After the v3.4 upgrade this function
-    //       will always return 100_00.
+    // @note This `initialize` function
+    //       must be executed *before* the main `Pool` contract is upgraded to v3.4.
+    //       This is necessary to fetch the dynamic `FLASHLOAN_PREMIUM_TO_PROTOCOL` value
+    //       from the v3.3 `Pool`'s storage. After the `Pool` contract is upgraded to v3.4,
+    //       its `FLASHLOAN_PREMIUM_TO_PROTOCOL` function will become a constant and always
+    //       return `100_00`, making the old dynamic value inaccessible.
     uint128 oldFlashloanPremiumToProtocol = _pool.FLASHLOAN_PREMIUM_TO_PROTOCOL();
     if (oldFlashloanPremiumToProtocol != 100_00) {
       emit FlashloanPremiumToProtocolUpdated({
--- src/VariableDebtTokenMainnetInstanceGHO.sol
+++ src/VariableDebtTokenMainnetInstanceGHO.sol
@@ -14,19 +14,21 @@ import {IVariableDebtTokenMainnetInstanceGHO} from "./interfaces/IVariableDebtTo
 contract VariableDebtTokenMainnetInstanceGHO is VariableDebtToken, IVariableDebtTokenMainnetInstanceGHO {
   uint256 public constant DEBT_TOKEN_REVISION = 4;
 
-  // These are additional variables that were in the v3.3 VToken for the GHO aToken
-  // but there is no such variables in all other vTokens in both v3.3 and v3.4
-  // so we need to clean them in case in future versions of vTokens it will be
-  // needed to add new storage variables.
-  // If we don't clean them, then the aToken for the GHO token will have non zero values
-  // in these new variables that may be added in the future.
+  // These are additional storage variables that were present in the v3.3 VariableDebtToken (vToken) implementation specific to GHO.
+  // However, such variables do not exist in other vToken implementations (for other assets) in either v3.3 or v3.4.
+  // Therefore, these slots need to be cleaned (zeroed out) in case future vToken versions
+  // require the addition of new storage variables at these specific slots.
+  // If these slots are not cleaned, the GHO vToken contract would retain non-zero values
+  // at these storage locations, potentially conflicting with new variables introduced in future standard vToken upgrades.
   address private _deprecated_ghoAToken;
   address private _deprecated_discountToken;
   address private _deprecated_discountRateStrategy;
 
-  // This global variable can't be cleaned. The future vToken code upgrades should consider
-  // that on this slot there can't be a new mapping because it holds some non-zero values
-  // On this slot there can be only value types, not reference types.
+  // This mapping variable (`_deprecated_ghoUserState`, now commented out) cannot be 'deleted' in the same way as simple value types.
+  // If it previously held data, those storage slots remain occupied.
+  // Future vToken upgrades must be mindful of this: this specific storage slot (where the mapping was declared)
+  // should not be reused for a new mapping due to potential data remnants.
+  // It might be possible to reuse it for simple value types, but not for new reference types like another mapping.
   // mapping(address => GhoUserState) internal _deprecated_ghoUserState;
 
   constructor(IPool pool, address rewardsController) VariableDebtToken(pool, rewardsController) {}
@@ -45,9 +47,9 @@ contract VariableDebtTokenMainnetInstanceGHO is VariableDebtToken, IVariableDebt
     string memory debtTokenSymbol,
     bytes calldata params
   ) external override initializer {
-    // @note this is the default initialization function
-    // the same as the `VariableDebtTokenInstance.initialize` function
-    // but contains the additional logic for deleting the deprecated variables
+    // @note This is the standard initialization function,
+    //       similar to the standard `VariableDebtToken.initialize` function,
+    //       but it includes additional logic to delete the deprecated storage variables specific to the old GHO vToken.
 
     delete _deprecated_ghoAToken;
     delete _deprecated_discountToken;
