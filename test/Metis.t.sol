d4ceaa21c5038c5c3eaeacdfc2d8b9d7d47f9649
Nikita <32309387+TepNik@users.noreply.github.com>
2025-07-21 19:02:01 +0800

    feat: update execution blocks (#3)
    * Updated execution blocks, removed v3.4 upgrade execution
    
    * Fixed some tests
    
    * Updated submodule

--- test/Metis.t.sol
+++ test/Metis.t.sol
@@ -1,22 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// TODO remove after v3.4 go live
-import {GovV3Helpers} from "aave-helpers/src/GovV3Helpers.sol";
-
 import {DeploymentLibrary} from "../script/Deploy.s.sol";
 
 import {UpgradeTest} from "./UpgradeTest.t.sol";
 import {Deployments} from "../src/Deployments.sol";
 
-contract MetisTest is UpgradeTest("metis", 20752122) {
-  function setUp() public override {
-    super.setUp();
-
-    // TODO remove after v3.4 go live
-    GovV3Helpers.executePayload(vm, 40);
-  }
-
+contract MetisTest is UpgradeTest("metis", 20786990) {
   function _getPayload() internal virtual override returns (address) {
     return DeploymentLibrary._deployMetis();
   }
--- test/Optimism.t.sol
+++ test/Optimism.t.sol
@@ -1,22 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// TODO remove after v3.4 go live
-import {GovV3Helpers} from "aave-helpers/src/GovV3Helpers.sol";
-
 import {DeploymentLibrary} from "../script/Deploy.s.sol";
 
 import {UpgradeTest} from "./UpgradeTest.t.sol";
 import {Deployments} from "../src/Deployments.sol";
 
-contract OptimismTest is UpgradeTest("optimism", 137876578) {
-  function setUp() public override {
-    super.setUp();
-
-    // TODO remove after v3.4 go live
-    GovV3Helpers.executePayload(vm, 77);
-  }
-
+contract OptimismTest is UpgradeTest("optimism", 138140906) {
   function _getPayload() internal virtual override returns (address) {
     return DeploymentLibrary._deployOptimism();
   }
